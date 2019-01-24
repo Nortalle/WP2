7ae1cdf6e583a4bdc39fa88136128de73cef11f0
Unknown <lloysekrug@gmail.com>
2019-01-24 20:13:40 +0100

    We can get a badge recently created

--- swagger/fruits-specs/src/test/java/io/AMT/gamification/api/spec/steps/CRUDBadgeSteps.java
+++ swagger/fruits-specs/src/test/java/io/AMT/gamification/api/spec/steps/CRUDBadgeSteps.java
@@ -8,6 +8,12 @@ import io.AMT.gamification.ApiException;
 import io.AMT.gamification.ApiResponse;
 import io.AMT.gamification.api.BadgesApi;
 import io.AMT.gamification.api.dto.BadgeWrite;
+import io.AMT.gamification.api.dto.BadgeRead;
+import org.junit.Assert;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertEquals;
@@ -18,11 +24,18 @@ public class CRUDBadgeSteps {
     private BadgesApi badgesApi;
 
     private BadgeWrite badgesWrite;
+    private BadgeRead badgeRead;
 
     private ApiResponse lastApiResponse;
     private ApiException lastApiException;
     private boolean lastApiCallThrewException;
     private int lastStatusCode;
+    private long badgeId;
+
+
+    private String token1 = "token1";
+    private String wrongToken = "token2";
+
 
     public CRUDBadgeSteps(Environment environment){
         this.environment = environment;
@@ -39,16 +52,20 @@ public class CRUDBadgeSteps {
         badgesWrite = new BadgeWrite();
         assertNotNull(badgesWrite);
         badgesWrite.setVisible(true);
+        badgesWrite.setImage("testBadge");
+        badgesWrite.setName("testBadge");
     }
 
     @When("^I POST it to the /badges endpoint$")
     public void i_POST_it_to_the_badges_endpoint() throws Throwable {
         try {
-            String token = "salut";
-            lastApiResponse = badgesApi.createBadgeWithHttpInfo(token, badgesWrite);
+            lastApiResponse = badgesApi.createBadgeWithHttpInfo(token1, badgesWrite);
             lastApiCallThrewException = false;
             lastApiException = null;
             lastStatusCode = lastApiResponse.getStatusCode();
+
+            //get the id of the badge created
+            getIdFromLocation(lastApiResponse.getHeaders().get("Location").toString());
         } catch (ApiException e) {
             lastApiCallThrewException = true;
             lastApiResponse = null;
@@ -62,5 +79,44 @@ public class CRUDBadgeSteps {
     public void i_receive_a_status_code_for_badge_creation(int arg1) throws Throwable {
         assertEquals(201, lastStatusCode);
     }
+
+
+
+    @When("^I GET it to the /badges/badgeId endpoint$")
+    public void i_GET_it_to_the_badges_badgeId_endpoint() throws Throwable {
+        badgeRead = badgesApi.getBadge(badgeId, token1);
+    }
+
+    @Then("^I receive a response containing the badge$")
+    public void i_receive_a_response_containing_the_badge() throws Throwable {
+        BadgeRead expected = new BadgeRead();
+        expected.setId(badgeId);
+        expected.setImage("testBadge");
+        expected.setName("testBadge");
+        expected.setVisible(true);
+        assertEquals(expected, badgeRead);
+    }
+
+    /*
+    @When("^I GET it to the /badges/badgeId endpoint with wrong token$")
+    public void i_GET_it_to_the_badges_badgeId_endpoint_with_wrong_token() throws Throwable {
+        badgeRead = badgesApi.getBadge(badgeId, wrongToken);
+    }
+
+    @Then("^I a recieve a response containing a null$")
+    public void i_a_recieve_a_response_containing_a_null() throws Throwable {
+        Assert.assertNull(badgeRead);
+    }
+    */
+
+    private void getIdFromLocation(String location){
+        String[] segments = location.split("/");
+        String almostIdStr =  segments[segments.length-1];
+        String idStr = almostIdStr.substring(0,almostIdStr.length()-1);
+        badgeId = Long.parseLong(idStr);
+    }
+
+
+
 
 }
