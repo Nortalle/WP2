215d6df41e5ae3a4622c4296b42ecb5e80d9bd37
Nortalle <vincent.guidoux1@heig-vd.ch>
2019-01-25 01:19:43 +0100

    Amélioration de la suppression de badge

--- swagger/spring-server/src/main/java/io/AMT/gamification/repositories/RulesRepository.java
+++ swagger/spring-server/src/main/java/io/AMT/gamification/repositories/RulesRepository.java
@@ -3,6 +3,7 @@ package io.AMT.gamification.repositories;
 import io.AMT.gamification.entities.RuleEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface RulesRepository extends CrudRepository<RuleEntity, Long> {
@@ -10,4 +11,7 @@ public interface RulesRepository extends CrudRepository<RuleEntity, Long> {
     List<RuleEntity> findAllByApiKey(String apiKey);
 
     List<RuleEntity> findAllByApiKeyAndIfEventType(String apiKey, String type);
+
+    @Transactional
+    void deleteRuleEntitiesByBadge_Id(Long id);
 }
